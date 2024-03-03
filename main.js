96b20d5f173f7e4c352b39971acb055fe878f58f
tansenAngdembe <tansena54ang@gmail.com>
2024-03-03 09:02:45 +0545

    working with main.js file

--- main.js
+++ main.js
@@ -1,27 +1,55 @@
 const dice = document.getElementById("dice")
 let randomValue = document.getElementById("outPut")
 let players = document.getElementById("player")
+let points = document.getElementById("point")
+let colorValue = document.getElementById("color")
 
 
-let diceObj ={
-   1 : "Red",
-   2 : "Green",
-   3 : "blue",
-   4 : "white"
-};
-let click = 0;
 
 // dice setup  function
-const diceFunction = ()=>{
-    if(randomValue.innerText.trim() === "") randomValue.innerText = "Roll the dice";
-    if(players.innerText.trim() === "" ) {
-    }
-    dice.addEventListener("click",(val)=>{
-        val.preventDefault();
-        let randomNumber = Math.floor(Math.random() * 6) + 1;
-        randomValue.innerText = randomNumber
-   })
+const diceFunction = () => {
+   let click = 1;
+   let valueObj = {
+      color: "Red",
+      number: 0
+   }
 
+   if (randomValue.innerText.trim() === "") randomValue.innerText = "Roll the dice";
+   players.innerText = "Red"
+   const rollValue = (color) => {
+      players.innerText = color
+      valueObj.color = color
+   }     
+
+   dice.addEventListener("click", (val) => {
+      val.preventDefault();
+      click += 1;
+      if (click > 4) {
+         click = 1
+      }
+      let randomNumber = Math.floor(Math.random() * 6) + 1;
+      randomValue.innerText = randomNumber;
+      valueObj.number = randomNumber;
+      points.innerText = valueObj.number;
+      colorValue.innerText = valueObj.color;
+
+
+  
+      switch (click) {
+         case 1:
+            rollValue("Red")
+            break;
+         case 2:
+            rollValue("Green")
+            break;
+         case 3:
+            rollValue("Blue")
+            break;
+         case 4:
+            rollValue("Yellow")
+            break;
+      }
+   });
 }
 
 diceFunction()
